7bd20c9168662bec4069db8a6f116b404c0983c7
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-09-02 19:38:35 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ function reset_trace_err () {
     trace_err = 0
     trace_err_old = 0
 }
+function get_IR_Data() {
+    let IR_old = IR
+    for (let ir_no = 0 ; ir_no <= 4; ir_no ++) {
+        IR[ir_no] = BitRacer.readIR2(ir_no)
+    }
+}
 function trace_line (base_speed: number, Kp: number, Kd: number) {
     line_position = BitRacer.readLine()
     trace_err = 0 - line_position
@@ -11,15 +17,18 @@ function trace_line (base_speed: number, Kp: number, Kd: number) {
     BitRacer.motorRun(BitRacer.Motors.M_L, base_speed - PD_Value)
     BitRacer.motorRun(BitRacer.Motors.M_R, base_speed + PD_Value)
 }
+
+let IR: number[] = []
+let IR_old: number [] = []
+let PD_Value = 0
+let delta_err = 0
 let line_position = 0
 let trace_err_old = 0
 let trace_err = 0
-let PD_Value = 0
-let delta_err = 0
-let base_turn_speeed = 0
-let base_speed = 0
-let Kd = 0
 let Kp = 0
+let Kd = 0
+let base_speed = 0
+let base_turn_speeed = 0
 basic.forever(function () {
 	
 })

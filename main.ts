3559ae45590927d7b5e06b617ce9b4597a87c058
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-09-03 11:21:15 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 function detect_crossroad_type () {
-    let goal_timer: number = 0
     while (true) {
         IR_new = get_IR_Data()
         // 終點(8)
@@ -13,8 +12,8 @@ function detect_crossroad_type () {
             goal_timer = 0
         }
         // T字(3)或十字(7)
-        if (IR_new[0] < 500 && IR_old [0] > 1200 && IR_new[4] < 500 && IR_old [4] > 1200) {
-        	if (IR_old[2] > 1200 && IR_new[2] <500){
+        if (IR_new[0] < 500 && IR_old[0] > 1200 && IR_new[4] < 500 && IR_old[4] > 1200) {
+            if (IR_old[2] > 1200 && IR_new[2] < 500) {
                 crossroad_type = 3
                 break;
             } else {
@@ -22,8 +21,8 @@ function detect_crossroad_type () {
                 break;
             }
         }
-        // 左轉(1)或左卜(5) 
-        if (IR_new[0] < 500 && IR_old[0] > 1200 && IR_old[4] < 500){
+        // 左轉(1)或左卜(5)
+        if (IR_new[0] < 500 && IR_old[0] > 1200 && IR_old[4] < 500) {
             if (IR_new[2] > 1200) {
                 crossroad_type = 5
                 break;
@@ -34,7 +33,7 @@ function detect_crossroad_type () {
         }
         // 右轉(2)或右卜(6)
         if (IR_new[4] < 500 && IR_old[4] > 1200 && IR_new[0] < 500) {
-            if (IR_new[2] > 1200){
+            if (IR_new[2] > 1200) {
                 crossroad_type = 6
                 break;
             } else {
@@ -50,12 +49,20 @@ function detect_crossroad_type () {
         IR_old = IR_new
     }
 }
-function drive_car(mode: number) {
-    let move_ticks: number = 0          //直線區域移動次數
-    let total_move_ticks: number = 40   //直線區域預計可移動總次數
-    let turn_pwm: number = 390          //轉彎的PWM值
-    let turn_ticks:number = 0           //轉彎區域移動次數
-    let total_turn_ticks:number = 100   //轉彎區域預計可移動總次數
+function reset_trace_err () {
+    trace_err = 0
+    trace_err_old = 0
+}
+function trace_line (base_speed: number, Kp: number, Kd: number) {
+    line_position = BitRacer.readLine()
+    trace_err = 0 - line_position
+    delta_err = trace_err - trace_err_old
+    trace_err_old = trace_err
+    PD_Value = Kp * trace_err + Kd * delta_err
+    BitRacer.motorRun(BitRacer.Motors.M_L, base_speed - PD_Value)
+    BitRacer.motorRun(BitRacer.Motors.M_R, base_speed + PD_Value)
+}
+function drive_car (mode: number) {
     reset_trace_err()
     // mode == 0 直走
     if (mode == 0) {
@@ -65,7 +72,7 @@ function drive_car(mode: number) {
             trace_line(320, 250, 140)
             IR_new = get_IR_Data()
             move_ticks += 1
-            if (move_ticks > total_move_ticks && (IR_new[0] > 1200 || IR_new[4] > 1200 || (IR_new[1] < 500 && IR_new[2] < 500 && IR_new[3] < 500))) {
+            if (move_ticks > total_move_ticks && (IR_new[0] > 1200 || IR_new[4] > 1200 || IR_new[1] < 500 && IR_new[2] < 500 && IR_new[3] < 500)) {
                 basic.showString("X")
                 BitRacer.motorRun(BitRacer.Motors.All, 50)
                 break;
@@ -86,13 +93,12 @@ function drive_car(mode: number) {
                 turn_ticks = 0
                 while (true) {
                     trace_line(0, 220, 250)
-                    if (line_position <= 0.3 && line_position >= -0.3){
+                    if (line_position <= 0.3 && line_position >= -0.3) {
                         BitRacer.motorRun(BitRacer.Motors.All, 0)
                         break;
                     }
                 }
-                break
-                
+                break;
             }
         }
         BitRacer.LED(BitRacer.LEDs.LED_L, BitRacer.LEDswitch.off)
@@ -102,16 +108,16 @@ function drive_car(mode: number) {
     if (mode == 2) {
         basic.showString("R")
         BitRacer.LED(BitRacer.LEDs.LED_R, BitRacer.LEDswitch.on)
-        while(true) {
+        while (true) {
             BitRacer.motorRun(BitRacer.Motors.M_R, 0 - turn_pwm)
             BitRacer.motorRun(BitRacer.Motors.M_L, 0 + turn_pwm)
-            turn_ticks += 1            
+            turn_ticks += 1
             if (turn_ticks >= total_turn_ticks && BitRacer.readIR2(4) > 1200) {
                 basic.showString("C")
                 turn_ticks = 0
                 while (true) {
                     trace_line(0, 220, 250)
-                    if (line_position <= 0.3 && line_position >= -0.3){
+                    if (line_position <= 0.3 && line_position >= -0.3) {
                         BitRacer.motorRun(BitRacer.Motors.All, 0)
                         break;
                     }
@@ -127,16 +133,16 @@ function drive_car(mode: number) {
         basic.showString("U")
         BitRacer.LED(BitRacer.LEDs.LED_R, BitRacer.LEDswitch.on)
         BitRacer.LED(BitRacer.LEDs.LED_L, BitRacer.LEDswitch.on)
-        while(true) {
+        while (true) {
             BitRacer.motorRun(BitRacer.Motors.M_R, 0 - turn_pwm)
             BitRacer.motorRun(BitRacer.Motors.M_L, 0 + turn_pwm)
-            turn_ticks += 1            
+            turn_ticks += 1
             if (turn_ticks >= total_turn_ticks * 2 && BitRacer.readIR2(4) > 1200) {
                 basic.showString("C")
                 turn_ticks = 0
                 while (true) {
                     trace_line(0, 220, 250)
-                    if (line_position <= 0.3 && line_position >= -0.3){
+                    if (line_position <= 0.3 && line_position >= -0.3) {
                         BitRacer.motorRun(BitRacer.Motors.All, 0)
                         break;
                     }
@@ -146,26 +152,28 @@ function drive_car(mode: number) {
         }
         BitRacer.LED(BitRacer.LEDs.LED_L, BitRacer.LEDswitch.off)
         BitRacer.LED(BitRacer.LEDs.LED_R, BitRacer.LEDswitch.off)
-        basic.showIcon(IconNames.Happy)        
+        basic.showIcon(IconNames.Happy)
     }
     // mode == 4 停止
     if (mode == 4) {
         BitRacer.motorRun(BitRacer.Motors.All, 0)
     }
 }
-function reset_trace_err () {
-    trace_err = 0
-    trace_err_old = 0
-}
-function trace_line (base_speed: number, Kp: number, Kd: number) {
-    line_position = BitRacer.readLine()
-    trace_err = 0 - line_position
-    delta_err = trace_err - trace_err_old
-    trace_err_old = trace_err
-    PD_Value = Kp * trace_err + Kd * delta_err
-    BitRacer.motorRun(BitRacer.Motors.M_L, base_speed - PD_Value)
-    BitRacer.motorRun(BitRacer.Motors.M_R, base_speed + PD_Value)
-}
+input.onButtonPressed(Button.AB, function () {
+    // 校正IR感應器
+    basic.showString("*")
+    basic.pause(1000)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    BitRacer.CalibrateBegin()
+    // 雙輪PWM=250移動600ms
+    BitRacer.motorRun(BitRacer.Motors.All, 250)
+    basic.pause(600)
+    BitRacer.motorRun(BitRacer.Motors.All, 0)   
+    BitRacer.CalibrateEnd(BitRacer.LineColor.White)
+    basic.showIcon(IconNames.Heart)
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(200, music.beat(BeatFraction.Half))
+})
 function get_IR_Data () {
     let IR: number[] = []
     for (let ir_no = 0; ir_no <= 4; ir_no++) {
@@ -173,15 +181,26 @@ function get_IR_Data () {
     }
     return IR
 }
+// 直線區域移動次數
+let move_ticks = 0
+// 直線區域預計可移動總次數
+const total_move_ticks:number = 40
+// 轉彎區域移動次數
+let turn_ticks = 0
+// 轉彎區域預計可移動總次數
+const total_turn_ticks: number = 100
+// 轉彎的PWM值
+const turn_pwm: number = 390
 let PD_Value = 0
-let delta_err = 0
-let line_position: number = 0
-let trace_err_old = 0
-let trace_err = 0
-let base_turn_speeed = 0
-let base_speed = 0
-let Kd = 0
-let Kp = 0
-let crossroad_type: number = 0
-let IR_new: number[] = []
+let line_position:number = 0
+let trace_err_old:number = 0
+let trace_err:number = 0
+let delta_err:number = 0
 let IR_old: number[] = []
+let IR_new: number[] = []
+let crossroad_type: number = 0
+let goal_timer:number = 0
+let Kp:number = 0
+let Kd:number = 0
+let base_speed:number = 0
+
